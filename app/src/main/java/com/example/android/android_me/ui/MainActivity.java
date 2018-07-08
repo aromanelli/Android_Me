f4eb3cc74f04bd90e7bc6fe882bab73cab6a16a8
aromanelli <adrian.romanelli@gmail.com>
2018-07-08 16:07:19 -0700

    Lesson coding.

--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -18,13 +18,15 @@ package com.example.android.android_me.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.example.android.android_me.R;
 
 // This activity is responsible for displaying the master list of all images
 // TODO (4) Implement the MasterListFragment callback, OnImageClickListener
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MasterListFragment.OnImageSelectedListener {
 
+    private Toast prevToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +35,18 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    // TODO (5) Define the behavior for onImageSelected; create a Toast that displays the position clicked
+    @Override
+    public void imageSelected(int position) {
+        // Hate that old Toast won't go away when a new one is shown,
+        // so we cancel out old one ourselves if new one is incoming ...
+        // (assuming no threading issues of two images selected at same time)
+        if (prevToast != null) {
+            prevToast.cancel();
+        }
+        // TODO (5) Define the behavior for onImageSelected; create a Toast that displays the position clicked
+        prevToast = Toast.makeText(this, "Pos: " + position, Toast.LENGTH_SHORT);
+        prevToast.show();
+    }
+
 
 }
